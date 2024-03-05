8c70c333b5a6304e19f01c80f23b108b608166c4
xf-cpp <https://github.com/xf-cpp/yolopose.git>
2024-03-05 14:32:00 +0800

    3.5-14.31

--- exp/recordPath.py
+++ exp/recordPath.py
@@ -4,6 +4,8 @@ import numpy as np
 from PIL import Image
 from scipy.interpolate import CubicSpline
 from airsim import MultirotorClient, Vector3r, YawMode, Pose
+
+
 def getSpeed():
     global total_time, time_step, time_points, target_velocity
     # 创建系统模型
@@ -19,6 +21,8 @@ def getSpeed():
 
     # 插值得到平滑速度曲线
     target_velocity = cs(time_points_interp)
+
+
 def initAirsim():
     # 创建AirSim客户端
     client = airsim.MultirotorClient()
@@ -28,45 +32,51 @@ def initAirsim():
     # 上电
     client.armDisarm(True)
     return client
+
+
 def autoAction(client):
-# 创建深度图像捕获设置
-# depth_cam = airsim.ImageCaptureBase()
-# depth_cam.set_image_type(airsim.ImageType.DepthVis)
-# depth_cam.set_resolution(640, 480)  # 设置分辨率，根据需要调整
+    # 创建深度图像捕获设置
+    # depth_cam = airsim.ImageCaptureBase()
+    # depth_cam.set_image_type(airsim.ImageType.DepthVis)
+    # depth_cam.set_resolution(640, 480)  # 设置分辨率，根据需要调整
 
+    """
+    设置初始相对位置 录制双目相机数据  地图坐标轴西向为Y正，南向为X正，地面向下为Z正，坐标单位为m
+    第一个初始位置为底部的后置相机 坐标 -600，-50，-80
+    第二个初始位置为底部的前置相机 坐标-600，-50.25，-80
 
-    #设置到指定位置
+    """
+    # 设置到指定位置
     x_pos = -600
     y_pos = -50
     z_pos = -80
-    pose = airsim.Pose(airsim.Vector3r(x_pos,y_pos,z_pos),airsim.to_quaternion(0,0,0))
+    pose = airsim.Pose(airsim.Vector3r(x_pos, y_pos, z_pos), airsim.to_quaternion(0, 0, 0))
     client.simSetVehiclePose(pose, True)
-    #悬浮
+    # 悬浮
     client.moveByVelocityBodyFrameAsync(0, 0, 0, 1).join()
     client.hoverAsync().join()
     # 开始录制数据
     client.startRecording()
-    #往前跑五秒
+    # 往前跑五秒
     # client.moveByVelocityBodyFrameAsync(vx=10, vy=0,vz= 0, duration=5).join()
     # 停止录制数据
-    client.stopRecording()
+    # client.stopRecording()
 
-
-
-    # path = [airsim.Vector3r(x_pos,y_pos+200,z_pos),
-    #         airsim.Vector3r(x_pos + 200, y_pos+200, z_pos),
-    #         airsim.Vector3r(x_pos+200, y_pos, z_pos),
-    #         airsim.Vector3r(x_pos, y_pos, 0)
-    #         ]；；
-    # client.moveOnPathAsync(path,
-    #                        12, 120,
-    #                        airsim.DrivetrainType.ForwardOnly, airsim.YawMode(False, 0), 20, 1).join()
-    #悬浮
-    # client.moveByVelocityBodyFrameAsync(0, 0, 0, 1).join()
-    # client.hoverAsync().join()
+    path = [airsim.Vector3r(x_pos,y_pos+200,z_pos),
+            airsim.Vector3r(x_pos + 200, y_pos+200, z_pos),
+            airsim.Vector3r(x_pos+200, y_pos, z_pos),
+            airsim.Vector3r(x_pos, y_pos, 0)
+            ]
+    client.moveOnPathAsync(path,
+                           12, 120,
+                           airsim.DrivetrainType.ForwardOnly, airsim.YawMode(False, 0), 20, 1).join()
+    # 悬浮
+    client.moveByVelocityBodyFrameAsync(0, 0, 0, 1).join()
+    client.hoverAsync().join()
 
     # # 停止录制数据
     # client.stopRecording()
+
 
 if __name__ == "__main__":
     client = initAirsim()
